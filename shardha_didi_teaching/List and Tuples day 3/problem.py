9a48a631b386bc3ae6403a6fda599c715d2e1d98
Shivani-mali <shivanimali339@gmail.com>
2026-08-01 00:36:58 +0530

    again

--- shardha_didi_teaching/List and Tuples day 3/problem.py
+++ shardha_didi_teaching/List and Tuples day 3/problem.py
@@ -1,22 +1,22 @@
-# # 1. WAP to ask the user to enter names of their 3 favourite mocies and store then in a list
-# A = input("Enter the first movie name: ")
-# B = input("Enter the Second movie name: ")
-# C = input("Enter the Third movie name: ")
-# list = [A , B, C]
-# print("here is your list: \n", list )
+# 1. WAP to ask the user to enter names of their 3 favourite mocies and store then in a list
+A = input("Enter the first movie name: ")
+B = input("Enter the Second movie name: ")
+C = input("Enter the Third movie name: ")
+list = [A , B, C]
+print("here is your list: \n", list )
 
 
-# # or
-# Movies = []
-# A = input("Enter the first movie name: ")
-# B = input("Enter the Second movie name: ")
-# C = input("Enter the Third movie name: ")
+# or
+Movies = []
+A = input("Enter the first movie name: ")
+B = input("Enter the Second movie name: ")
+C = input("Enter the Third movie name: ")
 
-# Movies.append(A)
-# Movies.append(B)
-# Movies.append(C)
+Movies.append(A)
+Movies.append(B)
+Movies.append(C)
 
-# print(Movies)
+print(Movies)
 
 
 
