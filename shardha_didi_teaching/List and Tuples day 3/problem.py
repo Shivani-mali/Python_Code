d99401e150e2d116cd18bc69eb7e497b4f940d80
Shivani-mali <shivanimali339@gmail.com>
2026-08-01 00:35:51 +0530

    some files are added

--- shardha_didi_teaching/List and Tuples day 3/problem.py
+++ shardha_didi_teaching/List and Tuples day 3/problem.py
@@ -21,4 +21,31 @@
 
 
 # Question 2. WAP to check is a list contains a palindrome of elements.
-Palindrome = []
+list1 = [1, 2, 1]
+list2 = [1, 2, 3]
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("palindrome")
+else:
+    ("Not a palindrome")
+
+
+# WAP to count the no. of students with the "A" grade in the follwoing tuple:
+# Given tuple is ["C", "D", "A", "A", "A", "B", "B", "A"]
+
+Grades = ["C", "D", "A", "A", "A", "B", "B", "A"]
+
+print("Total A grades are: ", Grades.count("A"))
+
+
+# store and sort list them from A to D:
+tum = ["C", "D", "A", "A", "A", "B", "B", "A"]
+print(tum)
+tum.sort()
+print(tum)
+
+
+
